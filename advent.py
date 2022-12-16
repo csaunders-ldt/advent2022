4dea299f640953c18ebea2bdf9a50e48bf04a542
Chris Saunders <cjasaunders@outlook.com>
2022-12-16 21:57:07 +0000

    fix: strip input for python parser

--- advent.py
+++ advent.py
@@ -4,16 +4,14 @@ from importlib import import_module
 
 files = glob('day*/solve.py')
 files.sort(key=lambda f: match(r'day(\d+)[\\\/]*solve.py', f).group(1))
-print(files)
-
 module = import_module(files[0][:-3].replace('\\', '.').replace('/', '.'))
 
 part1Answer = open(files[0][:-8] + 'solutions.txt').read().splitlines()[-1]
-part1 = module.part1(open(files[0][:-8] + 'input.txt').read()[:-1])
-assert part1Answer == str(part1), 'Part 1 failed'
+part1 = module.part1(open(files[0][:-8] + 'input.txt').read().strip())
+assert part1Answer == str(part1), f'Part 1 failed, expected {part1Answer}, got {part1}'
 print('Part 1 passed')
 
 part2Answer = open(files[-1][:-8] + 'solutions2.txt').read().splitlines()[-1]
-part2 = module.part2(open(files[-1][:-8] + 'input.txt').read()[:-1])
-assert part2Answer == str(part2), 'Part 2 failed'
+part2 = module.part2(open(files[-1][:-8] + 'input2.txt').read().strip())
+assert part2Answer == str(part2), f'Part 2 failed, expected {part2Answer}, got {part2}'
 print('Part 2 passed')
