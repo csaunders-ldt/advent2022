61d9d4420b1c8c5f148f10ffc8efd410898489c5
csaunders-ldt <chris@letsdothis.com>
2022-12-19 17:18:45 +0000

    feat: part 2 (bad heuristic)

--- day19/index.ts
+++ day19/index.ts
@@ -1,22 +1,16 @@
 import {
-  every,
   filter,
+  flatMap,
   fromPairs,
   keys,
   map,
   mapValues,
   max,
-  memoize,
-  min,
   partial,
   reduce,
-  reverse,
   split,
   sum,
-  values,
-  zip,
 } from 'lodash';
-import { join } from 'path';
 import { solve } from '../utils';
 
 const types = ['geode', 'obsidean', 'clay', 'ore'] as const;
@@ -41,7 +35,6 @@ type State = {
   resources: Resources;
   robots: Resources;
   timeLeft: number;
-
   blueprint: Blueprint;
   maxCost: Resources;
 };
@@ -54,70 +47,71 @@ function timeToEarn(state: State, type: Resource, count: number) {
 
 function timeToBuildRobot(state: State, robot: keyof Resources) {
   const cost = state.blueprint[robot];
-  const times = map(keys(cost), (resource: Resource) =>
-    timeToEarn(state, resource, cost[resource]),
-  );
+  const times = map(keys(cost), (r: Resource) => timeToEarn(state, r, cost[r]));
   return max(times) + 1;
 }
 
 function canStart(state: State, type: keyof Resources) {
   const { robots, maxCost, timeLeft } = state;
-  if (robots[type] >= min([maxCost[type], 10]) && type !== 'geode')
-    return false;
+  if (robots[type] >= maxCost[type] && type !== 'geode') return false;
   return timeToBuildRobot(state, type) < timeLeft;
 }
 
 function afterBuilding(state: State, type: keyof Resources) {
   const buildTime = timeToBuildRobot(state, type);
-  const timeLeft = state.timeLeft - buildTime;
   const robots = { ...state.robots, [type]: state.robots[type] + 1 };
 
   const resources = mapValues(state.resources, (count, resource) => {
     const earned = buildTime * state.robots[resource];
     return count + earned - (state.blueprint[type][resource] || 0);
   });
-  return { ...state, resources, robots, timeLeft };
+  return { ...state, resources, robots, timeLeft: state.timeLeft - buildTime };
 }
 
 function getOptions(state: State): Resource[] {
-  if (state.timeLeft < 3 || canStart({ ...state, timeLeft: 1 }, 'geode'))
-    return ['geode'];
+  if (canStart({ ...state, timeLeft: 1 }, 'geode')) return ['geode'];
   return filter(types, partial(canStart, state));
 }
 
-function search(state: State): number {
-  const { resources, timeLeft, robots } = state;
-  let options = getOptions(state);
-  const score = robots.geode * timeLeft + resources.geode;
-
-  if (options.length === 0) return score;
-
-  return max(map(options, (type) => memoSearch(afterBuilding(state, type))));
+function next(state: State): State[] {
+  return map(getOptions(state), partial(afterBuilding, state));
 }
 
-const memoSearch = memoize(search, ({ resources, robots, timeLeft }: State) => {
-  return join([...values(resources), ...values(robots), timeLeft].toString());
-});
+function score({ robots, timeLeft, resources }: State): number {
+  return robots.geode * timeLeft + resources.geode;
+}
+
+function potential(state: State): number {
+  return score(state) + ((state.timeLeft + 1) * state.timeLeft) / 2;
+}
+
+function search(opts: State[], maxScore = 0): number {
+  while (opts.length) {
+    opts = filter(flatMap(opts, next), (s) => potential(s) > maxScore);
+    maxScore = max([...map(opts, score), maxScore]);
+  }
+  return maxScore;
+}
 
 function quality(blueprint: Blueprint, timeLeft = 24) {
   const resources = fromPairs(map(types, (type) => [type, 0])) as Resources;
   const robots = { ...resources, ore: 1 };
   const maxCost = mapValues(resources, (_, type) =>
-    max(map(types, (t2) => blueprint[t2][type] || 0)),
+    max(map(types, (t2) => blueprint[t2][type])),
   );
-  return search({ blueprint, resources, robots, timeLeft, maxCost });
+  return search([{ blueprint, resources, robots, timeLeft, maxCost }]);
 }
 
 function part1(blueprints: Blueprint[]) {
   return sum(map(blueprints, (b, i) => quality(b) * (i + 1)));
 }
 
-function part2(blueprints: Blueprint[]) {
-  return reduce(
-    blueprints.slice(0, 3),
-    (score, blueprint) => quality(blueprint, 32) * score,
-    1,
-  );
+function product(vals: number[]) {
+  return reduce(vals, (p, v) => p * v, 1);
 }
 
-solve({ part1, test1: 8, part2, test2: 62, parser });
+function part2(blueprints: Blueprint[]) {
+  return product(map(blueprints, (b) => quality(b, 32)));
+}
+
+solve({ part1, test1: 33, part2, test2: 56 * 62, parser });
