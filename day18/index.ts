647da3aad63cf9be024e87e1b19eab39cec2a673
csaunders-ldt <chris@letsdothis.com>
2022-12-20 14:01:59 +0000

    feat: day18

--- day18/index.ts
+++ day18/index.ts
@@ -1,67 +1,49 @@
-import {
-  cloneDeep,
-  countBy,
-  every,
-  filter,
-  flatMap,
-  forEach,
-  keyBy,
-  map,
-  split,
-  sum,
-  uniqBy,
-} from 'lodash';
+import { every, filter, flatMap, forEach, join, map, split, sum } from 'lodash';
 import { solve } from '../utils';
 
-type Point = [x: number, y: number, z: number];
+type Point = [x: number, y: number, z: number]; //idk its just easier
 
 function digits(line: string) {
-  return map([...line.match(/(-?\d+)/g)], Number);
+  return join([...line.match(/(-?\d+)/g)], ',');
 }
 
-function parser(input: string): Point[] {
-  return map(split(input, '\n'), digits) as Point[];
+function parser(input: string): Set<string> {
+  return new Set<string>(map(split(input, '\n'), digits));
 }
 
-function neighbours(axis: Point): Point[] {
-  return flatMap(axis, (dim, i) => [
-    [...axis.slice(0, i), dim - 1, ...axis.slice(i + 1)],
-    [...axis.slice(0, i), dim + 1, ...axis.slice(i + 1)],
-  ]) as Point[];
+function neighbours(point: string): Point[] {
+  const val = eval(`[${point}]`);
+  return flatMap<number, Point>(val, (dim, i) => [
+    [...val.slice(0, i), dim - 1, ...val.slice(i + 1)] as Point,
+    [...val.slice(0, i), dim + 1, ...val.slice(i + 1)] as Point,
+  ]);
 }
 
-function part1(points: Point[]) {
-  const names = new Set(map(points, String));
-  const exists = (v: Point) => names.has(v.toString());
-  const faces = (point: Point) => 6 - filter(neighbours(point), exists).length;
-  return sum(map(points, faces));
+function part1(points: Set<string>) {
+  const hasSeen = (n: Point) => points.has(n.toString());
+  const sides = (p: string) => 6 - filter(neighbours(p), hasSeen).length;
+  return sum(map([...points], sides));
 }
 
-function validNeighbours(point: Point) {
-  return filter(neighbours(point), (p) => every(p, (v) => v >= 0 && v <= 25));
+function validNeighbours(point: string) {
+  return filter(neighbours(point), (p) => every(p, (v) => v >= -1 && v <= 26));
 }
 
-function floodFill(searchSpace: Set<Point>, seen: Set<string>) {
-  if (searchSpace.size === 0) return seen;
-  const allNeighbours = flatMap([...searchSpace], validNeighbours);
-  const next = uniqBy(
-    filter(allNeighbours, (v) => !seen.has(String(v))),
-    String,
-  );
+function floodFill(todo: Set<string>, seen: Set<string>, blocks: Set<string>) {
+  if (todo.size === 0) return seen;
 
-  return floodFill(new Set(next), new Set(map([...seen, ...next], String)));
+  const allNeighbours = map(flatMap([...todo], validNeighbours), String);
+  const next = filter(allNeighbours, (v) => !seen.has(v) && !blocks.has(v));
+  forEach(allNeighbours, (v) => seen.add(v));
+  return floodFill(new Set(next), seen, blocks);
 }
 
-function part2(points: Point[]) {
-  const names = new Set(map(points, String));
-  const exists = (v: Point) => names.has(v.toString());
-  const faces = (point: Point) => 6 - filter(neighbours(point), exists).length;
-  const visiblePoints = floodFill(
-    new Set([points[0]]),
-    new Set(map(points, String)),
-  );
-  console.log(visiblePoints);
-  return sum(map(visiblePoints, faces));
+function part2(points: Set<string>) {
+  const seenSet = floodFill(new Set(['-1,-1,-1']), new Set(), points);
+  const hiddenPoints = filter([...points], (p) => !seenSet.has(p));
+  const isPoint = (n) => seenSet.has(n.toString()) && !points.has(n.toString());
+  const sides = (p: string) => filter(neighbours(p), isPoint).length;
+  return sum(map([...points], sides));
 }
 
-solve({ part1, test1: 66, part2, test2: 58, parser });
+solve({ part1, test1: 64, part2, test2: 58, parser });
