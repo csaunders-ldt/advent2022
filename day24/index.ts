8c2cdf31f87de89fbbfff86e9d063178d0266e43
Chris Saunders <cjasaunders@outlook.com>
2022-12-24 12:50:09 +0000

    feat: cleanup

--- day24/index.ts
+++ day24/index.ts
@@ -6,28 +6,20 @@ import {
   forEach,
   map,
   partial,
-  range,
   split,
+  times,
   uniqBy,
+  values,
 } from 'lodash';
 import { solve } from '../utils';
 
 type Point = [x: number, y: number];
-type Blizzard = {
-  char: '<' | '>' | '^' | 'v';
-  position: Point;
-};
-type Grid = {
-  height: number;
-  width: number;
-  blizzards: Blizzard[];
-  hash: Set<string>;
-};
-
+type Blizzard = { char: '<' | '>' | '^' | 'v'; position: Point };
+type Grid = { max: Point; blizzards: Blizzard[] };
 type State = [x: number, y: number, time: number];
 
 function hash(blizzards: Blizzard[]) {
-  return new Set<string>(map(blizzards, 'position').map(String));
+  return new Set(map(blizzards, 'position').map(String));
 }
 
 function parser(input: string): Grid {
@@ -38,38 +30,25 @@ function parser(input: string): Grid {
     });
   });
   const height = countBy(input, (char) => char === '\n').true - 1;
-  const width = input.indexOf('\n') - 2;
-  return { height, width, blizzards, hash: hash(blizzards) };
+  return { max: [input.indexOf('\n') - 2, height], blizzards };
 }
 
-function wrapTo(height: number, width: number, [x, y]: Point): Point {
+function wrapTo([width, height]: Point, [x, y]: Point): Point {
   return [((x % width) + width) % width, ((y % height) + height) % height];
 }
 
-function getGridAt({ blizzards, height, width }: Grid, time: number): Grid {
-  const wrap = partial(wrapTo, height, width);
-  const newBlizzards = map(blizzards, ({ position: [x, y], char }) => {
-    switch (char) {
-      case '<':
-        return { char, position: wrap([x - time, y]) };
-      case '>':
-        return { char, position: wrap([x + time, y]) };
-      case '^':
-        return { char, position: wrap([x, y - time]) };
-      case 'v':
-        return { char, position: wrap([x, y + time]) };
-    }
-  });
-  return { height, width, blizzards: newBlizzards, hash: hash(newBlizzards) };
+const delta = { '<': [-1, 0], '>': [1, 0], '^': [0, -1], v: [0, 1] };
+function next(blizzards: Blizzard[], max: Point): Blizzard[] {
+  return map(blizzards, ({ position: [x, y], char }) => ({
+    char,
+    position: wrapTo(max, [x + delta[char][0], y + delta[char][1]]),
+  }));
 }
 
 function pointsAround([x, y]: Point, height: number, width: number) {
-  const opts = flatMap([-1, 1], (d) => [
-    [x + d, y],
-    [x, y + d],
-  ]).concat([[x, y]]);
+  const opts = map(values(delta), ([dx, dy]) => [x + dx, y + dy]);
   return filter(
-    opts,
+    opts.concat([[x, y]]),
     ([x, y]) =>
       (x >= 0 && y >= 0 && x < width && y < height) ||
       (x === width - 1 && y === height) ||
@@ -79,19 +58,18 @@ function pointsAround([x, y]: Point, height: number, width: number) {
 
 function getSiblings(
   hashes: Set<String>[],
-  width: number,
-  height: number,
+  [width, height]: Point,
   [x, y, time]: State,
 ): State[] {
-  const gridHash = hashes[time];
-  const candidates = pointsAround([x, y], height, width);
-  const points = filter(candidates, (point) => !gridHash.has(String(point)));
+  const nearby = pointsAround([x, y], height, width);
+  const points = filter(nearby, (point) => !hashes[time].has(String(point)));
   return map(points, ([x, y]) => [x, y, time + 1]);
 }
 
 function timeTaken(grid: Grid, states: State[], end: Point) {
-  const grids = map(range(1000), (time) => getGridAt(grid, time + 1).hash);
-  const siblings = partial(getSiblings, grids, grid.width, grid.height);
+  let grids = [grid.blizzards];
+  times(1000, () => (grids = [...grids, next(grids.last, grid.max)]));
+  const siblings = partial(getSiblings, map(grids.slice(1), hash), grid.max);
 
   for (let time = 0; time < 1000; time++) {
     states = uniqBy(flatMap(states, siblings), String);
@@ -100,15 +78,19 @@ function timeTaken(grid: Grid, states: State[], end: Point) {
   throw new Error('Not found');
 }
 
+function end({ max: [x, y] }: { max: Point }): Point {
+  return [x - 1, y];
+}
+
 function part1(grid: Grid) {
-  return timeTaken(grid, [[0, -1, 0]], [grid.width - 1, grid.height]);
+  return timeTaken(grid, [[0, -1, 0]], end(grid));
 }
 
 function part2(grid: Grid) {
-  const end: Point = [grid.width - 1, grid.height];
-  const there = timeTaken(grid, [[0, -1, 0]], end);
-  const backAgain = timeTaken(grid, [[...end, there]], [0, -1]);
-  return there + backAgain + timeTaken(grid, [[0, -1, there + backAgain]], end);
+  const there = timeTaken(grid, [[0, -1, 0]], end(grid));
+  const back = timeTaken(grid, [[...end(grid), there]], [0, -1]);
+  const again = timeTaken(grid, [[0, -1, there + back]], end(grid));
+  return there + back + again;
 }
 
 solve({ part1, test1: 18, part2, test2: 54, parser });
