7039bf6069dfbb829af1de57fac2cfec0fe40c92
Chris Saunders <cjasaunders@outlook.com>
2022-12-26 00:10:59 +0000

    feat: tidy up link

--- day22/index.ts
+++ day22/index.ts
@@ -1,13 +1,16 @@
 import {
   clone,
+  every,
   filter,
   find,
   findIndex,
   flatMap,
   forEach,
   groupBy,
+  identity,
   map,
   mapValues,
+  partial,
   range,
   split,
 } from 'lodash';
@@ -16,7 +19,7 @@ import { solve } from '../utils';
 type Cell = {
   x: number;
   y: number;
-  isFilled: boolean;
+  value: string;
   next: State[];
 };
 type State = { cell: Cell; facing: number };
@@ -25,51 +28,52 @@ type Input = { cells: Cell[]; instructions: string[] };
 function parser(input: string): Input {
   const [grid, lines] = split(input, '\n\n');
   const maybeCells = flatMap(split(grid, '\n'), (row, y) =>
-    map(split(row, ''), (cell, x) => ({ x, y, cell, next: [] })),
+    map(split(row, ''), (value, x) => ({ x, y, value, next: [] })),
   );
-  const items = filter(maybeCells, ({ cell }) => cell !== ' ');
-  const cells = map(items, ({ cell, ...c }) => ({
-    ...c,
-    isFilled: cell === '#',
-  }));
+  const cells = filter(maybeCells, ({ value }) => value !== ' ');
   const instructions = lines.match(/([A-Z]+|\d+)/g);
   return { cells, instructions };
+}
+
+function rotate(value: number, turns: number): number {
+  return (value + turns + 4) % 4;
 }
 
 function move(state: State, distance: number): State {
   if (distance === 0) return state;
   const next = state.cell.next[state.facing];
-  return next.cell.isFilled ? state : move(next, distance - 1);
+  return next.cell.value === '#' ? state : move(next, distance - 1);
 }
 
 function execute(input: Input, state: State) {
-  forEach(input.instructions, (instruction, i) => {
+  forEach(input.instructions, (instruction) => {
     if (instruction === 'L' || instruction === 'R') {
-      state.facing = (state.facing + (instruction === 'L' ? -1 : 1) + 4) % 4;
-      return;
+      state.facing = rotate(state.facing, instruction === 'L' ? -1 : 1);
+    } else {
+      state = mapValues(move(state, +instruction), clone) as State;
     }
-    state = mapValues(move(state, +instruction), clone) as State;
   });
   return (state.cell.y + 1) * 1000 + (state.cell.x + 1) * 4 + state.facing;
 }
 
-function makeLoop({ cells }: Input) {
-  const cols = groupBy(cells, 'x');
-  const rows = groupBy(cells, 'y');
+function getNext(cell: Cell, loop = false, cells: Cell[], reverse = false) {
+  const alt = loop ? (reverse ? cells.last : cells[0]) : null;
+  return cells[findIndex(cells, cell) + (reverse ? -1 : 1)] || alt;
+}
+
+function link({ cells }: Input, loop = false) {
+  const [cols, rows] = [groupBy(cells, 'x'), groupBy(cells, 'y')];
   forEach(cells, (cell) => {
-    const col = cols[cell.x];
-    const row = rows[cell.y];
-    cell.next = [
-      { cell: row[findIndex(row, cell) + 1] || row[0], facing: 0 },
-      { cell: col[findIndex(col, cell) + 1] || col[0], facing: 1 },
-      { cell: row[findIndex(row, cell) - 1] || row.last, facing: 2 },
-      { cell: col[findIndex(col, cell) - 1] || col.last, facing: 3 },
-    ];
+    const [col, row] = [cols[cell.x], rows[cell.y]];
+    const get = partial(getNext, cell, loop);
+    const next = [get(row), get(col), get(row, true), get(col, true)];
+    cell.next = map(next, (position, facing) => ({ cell: position, facing }));
+    cell.next = map(cell.next, (state) => (state.cell ? state : null));
   });
 }
 
 function part1(input: Input) {
-  makeLoop(input);
+  link(input, true);
   return execute(input, { facing: 0, cell: input.cells[0] });
 }
 
@@ -77,71 +81,35 @@ function isCorner({ next }: Cell, i: number, j: number) {
   return next[i] && !next[i].cell.next[j] && next[j] && !next[j].cell.next[i];
 }
 
-function attach(l: Cell, r: Cell, lFacing: number, rFacing: number) {
-  l.next[(lFacing + 1) % 4] = { cell: r, facing: (rFacing + 1) % 4 };
-  r.next[(rFacing + 3) % 4] = { cell: l, facing: (lFacing + 3) % 4 };
+function attach(l: Cell, r: Cell, lDir: number, rDir = rotate(lDir, 1)) {
+  l.next[rotate(lDir, 1)] = { cell: r, facing: rotate(rDir, 1) };
+  r.next[rotate(rDir, 3)] = { cell: l, facing: rotate(lDir, 3) };
+  if (!l.next[lDir] && !r.next[rDir]) return;
 
-  if (!l.next[lFacing]) {
-    if (!r.next[rFacing]) return;
-    lFacing = (lFacing + 3) % 4;
+  if (!l.next[lDir]) {
+    lDir = rotate(lDir, 3);
   } else {
-    l = l.next[lFacing].cell;
+    l = l.next[lDir].cell;
   }
-  if (!r.next[rFacing]) {
-    rFacing = (rFacing + 1) % 4;
+  if (!r.next[rDir]) {
+    rDir = rotate(rDir, 1);
   } else {
-    r = r.next[rFacing].cell;
+    r = r.next[rDir].cell;
   }
+  if (every(l.next, identity) || every(r.next, identity)) return;
 
-  if (
-    l === r ||
-    filter(l.next, (v) => v).length === 4 ||
-    filter(r.next, (v) => v).length === 4
-  ) {
-    return;
-  }
-  attach(l, r, lFacing, rFacing);
+  attach(l, r, lDir, rDir);
 }
 
 function fold(cell: Cell) {
-  let lFacing = find(range(4), (i) => isCorner(cell, i, (i + 1) % 4));
-
-  if (lFacing === undefined) return;
-
-  let rFacing = (lFacing + 1) % 4;
-  attach(cell.next[lFacing].cell, cell.next[rFacing].cell, lFacing, rFacing);
-}
-
-function makeLoopSimple({ cells }: Input) {
-  const cols = groupBy(cells, 'x');
-  const rows = groupBy(cells, 'y');
-  forEach(cells, (cell) => {
-    const col = cols[cell.x];
-    const row = rows[cell.y];
-    const next = [
-      row[findIndex(row, cell) + 1],
-      col[findIndex(col, cell) + 1],
-      row[findIndex(row, cell) - 1],
-      col[findIndex(col, cell) - 1],
-    ];
-    cell.next = map(next, (position, facing) =>
-      position
-        ? {
-            cell: position,
-            facing,
-          }
-        : null,
-    );
-  });
-}
-
-function wrap({ cells, ...rest }: Input) {
-  makeLoopSimple({ cells, ...rest }); // Good place to start;
-  forEach(cells, (cell) => fold(cell));
+  let lDir = find(range(4), (i) => isCorner(cell, i, rotate(i, 1)));
+  if (lDir === undefined) return;
+  attach(cell.next[lDir].cell, cell.next[rotate(lDir, 1)].cell, lDir);
 }
 
 function part2(input: Input) {
-  wrap(input);
+  link(input);
+  forEach(input.cells, fold);
   return execute(input, { facing: 0, cell: input.cells[0] });
 }
 
